55b399f5c55627f019be0d6b8c2106d8735703e7
peter stein <petersteinhro@gmail.com>
2023-09-15 08:47:05 +0200

    +cube item

---
+++ src/components/items/Cube.tsx
@@ -0,0 +1,11 @@
+import cube_logo from "../../assets/cube.png";
+
+const Cube = () => {
+  return (
+    <div className="cube">
+        <img src={cube_logo} alt="Cube Logo" />
+    </div>
+  )
+}
+
+export default Cube
