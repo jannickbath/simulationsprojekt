dc949faa67db53d435e64811fc40296b175bd2fc
peter stein <petersteinhro@gmail.com>
2023-09-15 08:59:14 +0200

    +target icon

--- src/components/Header/Target.tsx
+++ src/components/Header/Target.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import target_icon from "../../assets/target.png";
 
 const Target = () => {
   return (
-    <div>Target</div>
+    <div className="target">
+      <div className="icon">
+        <img src={target_icon} alt="Target Icon" />
+      </div>
+      <p>RightAlt</p>
+    </div>
   )
 }
 
