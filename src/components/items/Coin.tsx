307fd1d3ec89d2eb374a128b65ac16c3c7beea71
peter stein <petersteinhro@gmail.com>
2023-09-15 08:46:06 +0200

    +coin item

---
+++ src/components/items/Coin.tsx
@@ -0,0 +1,11 @@
+import coin_logo from "../../assets/coin.png";
+
+const Coin = () => {
+  return (
+    <div className="coin">
+        <img src={coin_logo} alt="Coin Logo" />
+    </div>
+  )
+}
+
+export default Coin
