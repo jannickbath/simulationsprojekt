3da2f4055a0fab18ed0aa5a17c2cbd23611e44f3
Jannick Bath <jannickbath@gmail.com>
2023-10-09 11:21:25 +0200

    *visuals

--- src/components/Zustand/Slices/textSlice.ts
+++ src/components/Zustand/Slices/textSlice.ts
@@ -2,9 +2,9 @@ import { State, StateCreatorFn, TextSlice } from '../Types';
 
 export const textSlice: StateCreatorFn<TextSlice> = (set) => ({
   text: {
-    text: "Lorem ipsum sit dolor amet.",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit non, ab aperiam error perspiciatis eius! Odit quisquam animi incidunt sed velit modi vel debitis et.",
     typedText: "",
-    remainingText: "Lorem ipsum sit dolor amet."
+    remainingText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit non, ab aperiam error perspiciatis eius! Odit quisquam animi incidunt sed velit modi vel debitis et."
   },
   updateTypedText: (newText) => set((state: State) => {
     return { text: { ...state.text, typedText: newText} }
