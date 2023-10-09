3a8e935f6eb3741c08c99123aafd0b625465ee66
Jannick Bath <jannickbath@gmail.com>
2023-10-09 10:34:22 +0200

    *model own car instead of using an image

--- src/components/Cars/Default.tsx
+++ src/components/Cars/Default.tsx
@@ -1,12 +1,25 @@
-import car_png from "../../assets/car_default.png";
-
-const Default = (props: Car) => {
+const Default = (props: any) => {
   const progress = props.progress + "%";
   
   return (
     <>
       <div className={`car car-default ${props.own ? "own" : ""}`} style={{left: progress}}>
-        <img src={car_png} alt="" />
+          <div className="car__body">
+            <div className="car__body__topper">
+              <div className="car__body__topper__front-glas"></div>
+              <div className="car__body__topper__back-glas"></div>
+            </div>
+            <div className="car__body__headlight"></div>
+            <div className="car__body__taillight"></div>
+            <div className="car__body__tires">
+              <div className="car__body__tires__front car__body__tires__tire">
+                <div className="car__body__tires__tire__inner"></div>
+              </div>
+              <div className="car__body__tires__back car__body__tires__tire">
+                <div className="car__body__tires__tire__inner"></div>
+              </div>
+            </div>
+          </div>
       </div>
     </>
   )
