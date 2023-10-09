c11f4fa584b72db753af6557ac980ff5b212480e
Jannick Bath <jannickbath@gmail.com>
2023-10-09 09:48:08 +0200

    *execute useProgressLoop() in main.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,8 @@ import PowerUpView from './PowerUpView'
 import Textbox from './Textbox'
 import Target from './Target'
 import Utility from './Utility'
-import { useProgressLoop } from '../../Game'
 
 const Header = () => {
-  useProgressLoop();
-
   return (
     <>
       <div className="header">
--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,26 @@ import ReactDOM from 'react-dom/client';
 import Tracks from './components/Tracks';
 import '../src/styles/css/main.css';
 import Header from './components/Header/Header';
+import { useProgressLoop } from './Game';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <div className="container">
-      <Header />
-      <Tracks />
-    </div>
-  </React.StrictMode>
-);
+const App = () => {
+  useProgressLoop();
 
-// Prevent spacebar from scrolling down
-document.addEventListener('keydown', (event: KeyboardEvent) => {
-  if (event.keyCode == 32) {
-    event.preventDefault();
-  }
-});
+  // Prevent spacebar from scrolling down
+  document.addEventListener('keydown', (event: KeyboardEvent) => {
+    if (event.keyCode === 32) {
+      event.preventDefault();
+    }
+  });
+
+  return (
+    <React.StrictMode>
+      <div className="container">
+        <Header />
+        <Tracks />
+      </div>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
