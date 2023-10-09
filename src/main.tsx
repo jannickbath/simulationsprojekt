e9ffe7c6920ccfc385428cb3d464e799ba967e38
Jannick Bath <jannickbath@gmail.com>
2023-10-09 12:00:48 +0200

    *log winner to console

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import Tracks from './components/Tracks';
 import '../src/styles/css/main.css';
 import Header from './components/Header/Header';
 import { useProgressLoop } from './Game';
+import { useBoundStore } from './components/Zustand/useBoundStore';
+import { Player } from './components/Zustand/Types';
 
 const App = () => {
   useProgressLoop();
+  const players = useBoundStore(state => state.players);
+  const cars = useBoundStore(state => state.cars);
+  const [winner, setWinner] = useState<null|Player>(null);
 
   // Prevent spacebar from scrolling down
   document.addEventListener('keydown', (event: KeyboardEvent) => {
@@ -14,6 +19,19 @@ const App = () => {
       event.preventDefault();
     }
   });
+  
+  useEffect(() => {
+    if (winner) return;
+
+    const winningCar = cars.find(car => car.progress === "100");
+    if (winningCar) {
+      const winningPlayer = players.find(player => player.carId == winningCar.id)
+      if (winningPlayer) {
+        console.log(winningPlayer);
+        setWinner(winningPlayer);
+      }
+    }
+  }, [cars, winner])
 
   return (
     <React.StrictMode>
