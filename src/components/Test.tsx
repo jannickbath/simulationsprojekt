2d89417df68dfc0aea6606781a5dee0b6015c32e
Jannick Bath <jannick.bath@lupcom.de>
2023-12-13 12:27:36 +0100

    *dynamic cypress component test

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react"
+
 const Test = () => {
+  const [text, setText] = useState("Blah");
+
   return (
-    <div className="customClass">Blah</div>
+    <div className="customClass" onClick={() => setText("")}>{text}</div>
   )
 }
 
--- src/tests/Test.cy.tsx
+++
@@ -1,9 +0,0 @@
-import Test from '../components/Test'
-import { mount } from 'cypress/react18'
-
-describe('<Test />', () => {
-  it('renders', () => {
-    mount(<Test />);
-    cy.get(".customClass").should("have.text", "Blah");
-  });
-})
